7511890f0a447004535d62df61b8ff11ec69c800
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-09-25 16:56:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <title>CityPlan Consultant </title>
+	  <link rel="shortcut icon" href="images/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
