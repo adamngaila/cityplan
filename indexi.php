2fdab41d320294ed4353675689784b928cdd0ba6
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-09-25 15:18:16 +0300

    Update indexi.php

--- indexi.php
+++ indexi.php
@@ -574,7 +574,7 @@ fidlayermaker.queryFeatures(query).then(result => result.features.forEach(t=>{
 	 
 	 ],(WebMap, MapView, Print, Search)=>{
 		 
-	   const map1 = new WebMap({"portalItem": {"id" : "7fe2f81759f842a2be27cf65ca5775d9"} });
+	   const map1 = new WebMap({"portalItem": {"id" : "16d587b61d8e430bb4582323c8918e40"} });
 	   
 	   const view = new MapView({container: "mapview", map: map1});
 	   //screenshot function
