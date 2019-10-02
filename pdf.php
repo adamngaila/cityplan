b6d04632cd541f18ef6bf63bdca42554290269b9
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-10-02 11:04:39 +0300

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-$connect = mysqli_connect("localhost","root","","cityplan");
+$connect = mysqli_connect("mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","h7l9ehepp73f4lp6","qn81i2ospadx0b5v","jvkaflsb5i15egxa");
 $message = '';
-
 
 
 $namba = $_SESSION["nam"];
