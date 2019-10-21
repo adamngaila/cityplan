40e6ab222be892717a71dd242c03d271df944ac4
adamngaila <38142076+adamngaila@users.noreply.github.com>
2019-10-21 14:09:09 +0300

    Update indexi.php

--- indexi.php
+++ indexi.php
@@ -1,8 +1,8 @@
 					<?php
-					$dbHost     = "	mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbUsername = "	h7l9ehepp73f4lp6";
-$dbPassword = "	qn81i2ospadx0b5v";
-$dbName     = "	jvkaflsb5i15egxa";
+					$dbHost     = "mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbUsername = "h7l9ehepp73f4lp6";
+$dbPassword = "qn81i2ospadx0b5v";
+$dbName     = "jvkaflsb5i15egxa";
 // Create database connection
 //$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
   $connect = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
